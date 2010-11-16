a89f1542a3b5ba62550d7d642d460df2bafafd06
papricek <patrikjira@gmail.com>
2010-11-16 15:29:08 +0100

    Functional crawling of multiple category url

---
+++ firminer.rb
@@ -0,0 +1,43 @@
+require "rubygems"
+require "anemone"
+require "fastercsv"
+
+puts "Firminer - data miner from directory Firmy.cz"
+
+categories = FasterCSV.read("data/categories.csv")
+
+puts "-- read categories file -- "
+
+categories.each do |row|
+  category_url = row[1].strip
+  category_ascii_name = category_url[/[^\/]+?$/]
+  file_name = "#{category_ascii_name}.csv"
+
+  begin
+  puts "-- crawling #{category_ascii_name} -- "
+  puts category_url
+  Anemone.crawl(category_url) do |anemone|
+
+    anemone.focus_crawl do |page|
+      unless page.url.to_s.include?("/detail/")
+        page.links.select { |link| link.to_s.include?(category_url) or link.to_s.include?("/detail/") }.reject { |link| link.to_s.include?("/reg/") }
+      else
+        []
+      end
+
+    end
+
+    file = File.new("data/#{file_name}", "w")
+    anemone.on_pages_like(/\/detail\//) do |page|
+      file.puts [page.url].to_csv
+    end
+
+    file
+
+    puts "-- Crawled #{@count} companies \n"
+  end
+
+  rescue
+    puts "-- #{category_ascii_name} failed to get crawled --"
+  end
+end
